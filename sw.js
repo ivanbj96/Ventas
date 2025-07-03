6c670d22285c361b8fb99f1eb5e4d337d22904af
ivanbj96 <64235647+ivanbj96@users.noreply.github.com>
2025-07-03 06:27:35 -0500

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js
+
+const CACHE_NAME = "tillup-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "TillUp.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
